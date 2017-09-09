d13f79ffb14c49008d914a8e48194c5360a51209
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-09-10 00:43:59 +0900

    [ADD]return message func

---
+++ manager/config/config.go
@@ -0,0 +1,27 @@
+package config_manager
+
+import "os"
+
+type configManager struct {
+	TGB_CHANNEL_SECRET       string
+	TGB_CHANNEL_ACCESS_TOKEN string
+	TGB_USER_ID              string
+}
+
+var sharedInstance *configManager = newConfigManager()
+
+func newConfigManager() *configManager {
+	cs := os.Getenv("TGB_CHANNEL_SECRET")
+	cat := os.Getenv("TGB_CHANNEL_ACCESS_TOKEN")
+	ui := os.Getenv("TGB_USER_ID")
+
+	if cs == "" || cat == "" || ui == "" {
+		panic("[FATAL]TGB_CHANNEL_SECRET: " + cs + " TGB_CHANNEL_ACCESS_TOKEN: " + cat + " TGB_USER_ID: " + ui)
+	}
+
+	return &configManager{cs, cat, ui}
+}
+
+func GetInstance() *configManager {
+	return sharedInstance
+}
