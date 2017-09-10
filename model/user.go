fb9a516ab1a5e6032d10715a2647a30917a07761
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-09-10 18:40:32 +0900

    [MODIFY]sql

--- model/user.go
+++ model/user.go
@@ -6,7 +6,7 @@ import (
 
 // 全て値があるときに利用する
 func InsertUser(db *sql.DB, userId string, region Region) error {
-	q := `insert into users (user_id, region) values (?,?)`
+	q := `insert into users (user_id, region) values ('?',?)`
 	_, err := db.Exec(q, userId, region)
 	if err != nil {
 		return err
@@ -16,7 +16,7 @@ func InsertUser(db *sql.DB, userId string, region Region) error {
 }
 
 func GetUserByUserId(db *sql.DB, userId string) (User, error) {
-	q := `select * from users where user_id=?`
+	q := `select * from users where user_id='?''`
 	var user User
 	//TODO: Regionはstring? int?
 	err := db.QueryRow(q, userId).Scan(&user.ID, &user.UserID, &user.Region)
